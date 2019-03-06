a619c0a0e0d209b4c7b8bc1e1f94f54720624e9a
中西匠 <takumi.n@no-win.local>
2019-03-06 22:59:37 +0900

    add help.sh

--- help.sh
+++ help.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-HELPER=`/Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType hud -windowPosition "lr" -title "Softwareupdate" -description "再起動の必要なアップデートがあります。アップデートと再起動を行いますか？" -button1 "はい" -button2 "後で" -cancelButton "2"`
+HELPER=`/Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType hud -windowPosition "lr" -title "Softwareupdate" -description "アップデート？" -button1 "はい" -button2 "後で" -cancelButton "2"`
 
       echo "jamf helper result was $HELPER";
 
